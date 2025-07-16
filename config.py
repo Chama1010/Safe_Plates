937de54d876f8e402f9e8f9304efdff1c83590b9
Chama1010 <ashenchamath1010@gmail.com>
2025-07-16 12:06:57 +0530

    5th commit minor

--- config.py
+++ config.py
@@ -1,7 +1,22 @@
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
+
+# class Config:
+#     SECRET_KEY = os.getenv('SECRET_KEY')
+#     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') 
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     EDAMAM_APP_ID = os.getenv('EDAMAM_APP_ID')
+#     EDAMAM_APP_KEY = os.getenv('EDAMAM_APP_KEY')
+
+
+import os
+
+# Only load .env locally (e.g., your dev machine)
+if os.environ.get("FLASK_ENV") != "production":
+    from dotenv import load_dotenv
+    load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
@@ -9,8 +24,6 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     EDAMAM_APP_ID = os.getenv('EDAMAM_APP_ID')
     EDAMAM_APP_KEY = os.getenv('EDAMAM_APP_KEY')
-
-
 
 
 
